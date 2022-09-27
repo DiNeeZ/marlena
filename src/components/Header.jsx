10d373fcebb83f840c4dfb5c0cc308a6cdd32cd5
DiNeeZ <dneez86@gmail.com>
2022-09-27 08:31:16 +0300

    layout added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,16 @@
+import Image from 'next/image'
 import React from 'react'
+import logoPic from '../assets/logo.svg'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div>
+      <div className='container mx-auto'>
+        <div>
+          <Image src={logoPic} alt='logo' />
+        </div>
+      </div>
+    </div>
   )
 }
 
