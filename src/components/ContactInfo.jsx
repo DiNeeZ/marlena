86e8daee0014eff89214f24936313683704084f6
DiNeeZ <dneez86@gmail.com>
2022-09-29 02:01:27 +0300

    added handy keyboard navigation in the header

--- src/components/ContactInfo.jsx
+++ src/components/ContactInfo.jsx
@@ -2,17 +2,21 @@ import { HiMail } from 'react-icons/hi'
 import { BsFillTelephoneFill } from 'react-icons/bs'
 
 const ContactInfo = ({ ...otherProps }) => {
-  
+
   return (
     <ul className={`${otherProps.className} text-[12px] flex-col text-sm items-end space-y-2`}>
-      <li className='w-full hover:text-violet-700 ease-in duration-150'>
-        <a className='flex justify-between leading-3 items-center space-x-4' href='tel:380482345355'>
+      <li className='w-full hover:text-violet-700  ease-in duration-150'>
+        <a
+          className='flex justify-between leading-3 items-center space-x-4 outline-none focus:text-violet-700'
+          href='tel:380482345355'>
           <BsFillTelephoneFill />
           <span>+38 (0482) 345 355</span>
         </a>
       </li>
-      <li className='w-full hover:text-violet-700 ease-in duration-150'>
-        <a className='flex justify-between leading-3 items-center space-x-4' href='mailto:marlena@renome-i.net'>
+      <li className='w-full hover:text-violet-700  ease-in duration-150'>
+        <a
+          className='flex justify-between leading-3 items-center space-x-4 outline-none focus:text-violet-700'
+          href='mailto:marlena@renome-i.net'>
           <HiMail className='w-4 h-4' />
           <span>marlena@renome-i.net</span>
         </a>
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -6,8 +6,8 @@ import logoPic from '../assets/logo.svg'
 const Logo = () => {
   return (
     <Link href={'/'}>
-      <a>
-        <div className='flex items-end space-x-2 font-lateef py-4 text-green-700'>
+      <a className=' outline-none text-green-700 focus:text-violet-700'>
+        <div className='flex items-end space-x-2 font-lateef py-4'>
           <Image src={logoPic} height={50} width={50} alt='logo' />
           <div className='flex flex-col space-y-1'>
             <p className='uppercase text-5xl font-medium leading-6 drop-shadow-xl'>Marlena</p>
